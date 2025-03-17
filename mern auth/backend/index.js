4b947da0b8a47ab1ee0658d3c027474d01b3275a
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-18 00:36:28 +0600

    fix issue

--- mern auth/backend/index.js
+++ mern auth/backend/index.js
@@ -18,6 +18,6 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port: " + PORT)
+    console.log("Server is running on port:" + PORT)
 })
 
