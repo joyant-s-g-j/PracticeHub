37f1f164ced71328b3c7f0088df9f0af7611113f
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-18 00:04:43 +0600

    file setup

--- mern auth/backend/db/connectDB.js
+++ mern auth/backend/db/connectDB.js
@@ -5,7 +5,7 @@ export const connectDB = async () => {
         const conn = await mongoose.connect(process.env.MONGO_URI)
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
-        console.log("MongoDB connection error: ", error)
+        console.log("MongoDB connection error : ", error)
         process.exit(1) // 1 is failure, 0 status code is success
     }
 };
