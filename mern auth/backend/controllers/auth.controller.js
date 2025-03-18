00715c619e38180c192b815f7ce58adac6d464f8
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-19 00:14:40 +0600

    email sent successfully

--- mern auth/backend/controllers/auth.controller.js
+++ mern auth/backend/controllers/auth.controller.js
@@ -29,6 +29,8 @@ export const signup = async (req, res) => {
         //jwt
         generateTokenAndSetCookie(res,user._id)
 
+        sendVerificationEmail(user.email.replace, verificationToken);
+
         res.status(201).json({
             success: true,
             message: "User Created Successfully",
