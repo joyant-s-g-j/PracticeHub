8b58137779458730e4953fbd06849bbffb86b845
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-20 02:57:27 +0600

    all routes done

--- mern auth/backend/index.js
+++ mern auth/backend/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./db/connectDB.js";
-
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js"
 
 dotenv.config(); // for solving crashed
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5001
 
 app.use(express.json()); //allows us to parse incoming requests:req.body
-
+app.use(cookieParser()); // allows us to parse incoming cookies
 // app.get("/", (req, res) => {
 //     res.send("Hello World 123");
 // }) for testing
--- mern auth/backend/routes/auth.route.js
+++ mern auth/backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { login, logout, signup, verifyEmail, forgotPassword, resetPassword } from "../controllers/auth.controller.js"; // must added the extension
+import { login, logout, signup, verifyEmail, forgotPassword, resetPassword, checkAuth } from "../controllers/auth.controller.js"; // must added the extension
+import { verifyToken } from "../middleware/verifyToken.js";
 
 
 const router = express.Router()
@@ -13,6 +14,7 @@ const router = express.Router()
 // router.get("/logout", (req, res) => {
 //     res.send("logout route");
 // });
+router.get("/check-auth", verifyToken, checkAuth)
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
