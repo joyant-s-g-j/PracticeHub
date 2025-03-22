c05a2e651a18810e0d21f07d3edb098189ee5547
joyantgupta <joyantsheikharguptajoy@gmail.com>
2025-03-22 06:18:43 +0600

    background done

--- mern auth/frontend/src/App.jsx
+++ mern auth/frontend/src/App.jsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom"
+import FloatingShape from "./components/FloatingShape"
+import SignupPage from "./pages/SignupPage"
+import LoginPage from "./pages/LoginPage"
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-red-500 text-5xl'>Hello World</h1>
-    </>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape color="bg-green-500" size="w-64 h-64" top="5%" left="15%" delay={0} />
+      <FloatingShape color="bg-emerald-500" size="w-48 h-48" top="65%" left="75%" delay={5} />
+      <FloatingShape color="bg-lime-500" size="w-32 h-32" top="35%" left="5%" delay={2} />
+
+      <Routes>
+        <Route path="/" element={"Home"}/>
+        <Route path="/signup" element={<SignupPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ mern auth/frontend/src/components/input.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const input = ({icon:Icon, ...props}) => {
+  return (
+    <div className='relative mb-6'>input</div>
+  )
+}
+
+export default input
